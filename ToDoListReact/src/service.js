601a01e6a6e8ca0355ee132fdb52577eef4f81a3
Bluma Berkowicz <144212051+BlumaBerkowicz@users.noreply.github.com>
2025-01-16 12:23:14 +0200

    Update service.js

--- ToDoListReact/src/service.js
+++ ToDoListReact/src/service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:5230/';
+axios.defaults.baseURL = process.env.API;
 const addErrorInterceptor= () => {
   axios.interceptors.response.use(
     (response) => {
